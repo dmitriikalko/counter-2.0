716f8baa72f0c97ceff3b6c3bd88dfab7966edcf
dmitriikalko <mitya.kalcko@yandex.ru>
2023-08-12 20:18:32 +0500

    добавлена кнопка увеличения счетчика и добавлен счетчик

--- counter 2.0/ViewController.swift
+++ counter 2.0/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var CounterNum: Int = 0
+    
+    @IBOutlet weak var CounterPlusButton: UIButton!
+    @IBOutlet weak var CounterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func PlusOneOnLabel(_ sender: Any) {
+        CounterNum += 1
+        CounterLabel.text = "значение счетчика теперь: \(CounterNum)"
+    }
+    
 
 }
 
